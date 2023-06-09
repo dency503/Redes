4a91f4deacb614694371257f97b06259182938e6
Dennis <dennisgodinez64@gmail.com>
2023-06-08 23:25:20 -0600

    Git

--- public/deploy.php
+++ public/deploy.php
@@ -1,44 +1,45 @@
 <?php
-    /**
-     * GIT DEPLOYMENT SCRIPT
-     *
-     * Used for automatically deploying websites via GitHub
-     *
-     */
-
-    // array of commands
-    $commands = array(
-        'echo $PWD',
-        'whoami',
-        'git pull',
-        'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
-    );
-
-    // exec commands
-    $output = '';
-    foreach($commands AS $command){
-        $tmp = shell_exec($command);
-        
-        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
-        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
-    }
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 */
+	// The commands
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git reset --hard HEAD',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+	// Make it pretty for manual user access (and why not?)
 ?>
-
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-    <meta charset="UTF-8">
-    <title>GIT DEPLOYMENT SCRIPT</title>
+	<meta charset="UTF-8">
+	<title>GIT DEPLOYMENT SCRIPT</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<div style="width:700px">
-    <div style="float:left;width:350px;">
-    <p style="color:white;">Git Deployment Script</p>
-    <?php echo $output; ?>
-    </div>
-</div>
+<pre>
+ ____________________________
+|                            |
+| Git Deployment Script v0.1 |
+|      github.com/riodw 2019 |
+|____________________________|
+
+<?php echo $output; ?>
+</pre>
 </body>
 </html>
